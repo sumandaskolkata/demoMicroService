99c82b0e0f0568eaf8c40d1b4f3ab9ae11cc9935
sumandaskolkata <sumandas.kolkata@yahoo.com>
2016-08-16 20:50:00 +0530

    initial commit with besic gradle structure

---
+++ src/main/java/hello/App.java
@@ -0,0 +1,28 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class App{
+    @RequestMapping("/helloworld")
+    String home(){
+        return "hello world";
+    }
+    @RequestMapping("/")
+    String rout(){
+        return "hello///////";
+    }
+
+
+    @RequestMapping("/login")
+    String login(){
+        return "hello world";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
